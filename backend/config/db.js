942ce048cca40fa7d4f447081e2c9228963647c1
Tanishk Sarathe <tanishksarathe19112@gmail.com>
2026-05-10 10:09:53 +0530

    Added Files

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("DB Error", err);
+  } else {
+    console.log("Database Connected");
+  }
+});
+
+module.exports = connection;
